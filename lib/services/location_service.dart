ae18a74241a25a94b6fa9a457ad722d85612b75f
F4JAM <jabrebock@free.fr>
2026-02-02 14:46:44 +0100

    Update location_service.dart
    Suppression de lignes de code commentées

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -53,34 +53,3 @@ class LocationService {
         .map((pos) => LatLng(pos.latitude, pos.longitude));
   }
 }
-
-
-/* MODIFICATION A FAIRE DANS mission_map.dart
-//1) Importer le service : OK
-      import '../services/location_service.dart';
-//2) Modifier l'initState()
-      @override
-      void initState() {
-        super.initState();
-
-       // Récupérer la position initiale
-        LocationService.instance.determinePosition().then((latLng) {
-        setState(() {
-            _currentLocation = latLng;
-         });
-        _mapController.move(latLng, 15);
-        });
-
-      // Écouter la position en temps réel
-      LocationService.instance.getPositionStream().listen((latLng) {
-        setState(() => _currentLocation = latLng);
-        });
-      }
-//3) _userCurrentLocation() reste identique
-//4)  supprimer toute la gestion d'autorisation
-toute la gestion d’autorisation
-     _determinePosition
-    _getLocation
-    _positionStream
-    locationSettings
- */
